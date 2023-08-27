75867c52ed35a4fd22c100d395cb97c6dc426077
ahad1033 <a.ahad2232@gmail.com>
2023-08-28 02:12:00 +0600

    basic setup done

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT || 5000
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+app.get('/', (req, res) => {
+  res.send('AirCNC Server is running..')
+})
+
+app.listen(port, () => {
+  console.log(`AirCNC is running on port ${port}`)
+})
